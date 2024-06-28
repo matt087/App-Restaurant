49b6d29309df5ce632dcc779665d1e5815abeaeb
JuanFraDev <jdonosoo@est.ups.edu.ec>
2024-06-28 00:30:29 -0500

    Contacto avanzado

--- frontend/src/app/components/contact/contact.component.ts
+++ frontend/src/app/components/contact/contact.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  socialMedia = [
+    { icon: 'facebook', name: 'Facebook', link: 'https://www.facebook.com' },
+    { icon: 'twitter', name: 'Twitter', link: 'https://www.twitter.com' }
+    // Agrega más según sea necesario
+  ];
 
+  phoneNumbers = [
+    { icon: 'phone', number: '+1-123-456-7890' },
+    { icon: 'phone', number: '+1-987-654-3210' }
+    // Agrega más según sea necesario
+  ];
 }
