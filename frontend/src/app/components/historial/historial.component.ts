fe599937f96d26738077fe3830aaae04a24dbbca
JuanFraDev <jdonosoo@est.ups.edu.ec>
2024-07-19 11:27:45 -0500

    Front end historial de pedidos

--- frontend/src/app/components/historial/historial.component.ts
+++ frontend/src/app/components/historial/historial.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface HistorialData {
+  fecha: Date;
+  nombre: string;
+  calificacion: string;
+  comentario: string;
+}
+
+const ELEMENT_DATA: HistorialData[] = [
+  { fecha: new Date(), nombre: 'Juan Pérez', calificacion: 'Excelente', comentario: 'El mejor servicio que he visto' },
+  { fecha: new Date(), nombre: 'Ana Gómez', calificacion: 'Muy bueno', comentario: 'Podría mejorar pero estuvo muy bien' },
+  // Agrega más datos de ejemplo según sea necesario
+];
 
 @Component({
   selector: 'app-historial',
   templateUrl: './historial.component.html',
-  styleUrl: './historial.component.css'
+  styleUrls: ['./historial.component.css']
 })
-export class HistorialComponent {
+export class HistorialComponent implements OnInit {
+  displayedColumns: string[] = ['fecha', 'nombre', 'calificacion', 'comentario'];
+  dataSource = new MatTableDataSource<HistorialData>(ELEMENT_DATA);
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
